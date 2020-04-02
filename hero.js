fec9614047f5c7aba56b1778bf2ffe27a06e3a6a
mj <hi.minjungkim@gmail.com>
2020-04-02 20:17:19 +0900

    Moving hero only inside game  background

--- hero.js
+++ hero.js
@@ -13,17 +13,18 @@ function init() {
     
     document.addEventListener("keydown", function(e){
         if (e.keyCode === 37) { // 왼쪽
-            // console.log('왼쪽');
+            if (hero.style.left === "-2.5px") { return }
             hero.style.background = "url('./images/hero.png') 70px";
             heroLocation -= 5;
+            // console.log(heroLocation);
             hero.style.left = `${heroLocation}px`;
         }  else if (e.keyCode === 39) { // 오른쪽
-            // console.log('오른쪽');
+            if (hero.style.left === "767.5px") { return }
             hero.style.background = "url('./images/hero.png') 35px"; 
             heroLocation += 5;
             hero.style.left = `${heroLocation}px`;
         }
-        
+            
         if (e.keyCode === 38) { // 위
             hero.style.background = "url('./images/hero.png') 105px"; 
         } else if (e.keyCode === 40) { // 아래
