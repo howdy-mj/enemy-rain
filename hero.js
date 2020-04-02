8d9d6798aa558537eff4901d786da88806820cc4
mj <hi.minjungkim@gmail.com>
2020-04-02 17:03:15 +0900

    Turn hero

--- hero.js
+++ hero.js
@@ -1,17 +1,29 @@
-// class Hero {
-  
-// }
+const bg = document.querySelector("#bg");
+const div = document.createElement("div");
 
-
-function getHero() {
-    const div = document.createElement("div");
-    const bg = document.querySelector("#bg");
-    // console.log(bg);
+function turnFront() {
     div.className = "hero";
-    bg.append(div);
+    bg.append(div);    
 }
 
 function init() {
-    getHero();
+    turnFront();
+    
+    document.addEventListener("keydown", function(e){
+        if (e.keyCode === 37 && div.offsetLeft > 0) {
+            // 왼쪽
+            div.style.background = "url('./images/hero.png') 70px";
+        } else if (e.keyCode === 38) {
+            // 위
+            div.style.background = "url('./images/hero.png') 105px"; 
+        } else if (e.keyCode === 39) {
+            // 오른쪽
+            div.style.background = "url('./images/hero.png') 35px"; 
+        } else if (e.keyCode === 40) {
+            // 아래
+            div.style.background = "url('./images/hero.png') 140px"; 
+        }
+    });
+
 }
 init();
