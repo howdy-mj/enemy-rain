b65922c151550c2d443e27f898f448b071dc47f3
mj <hi.minjungkim@gmail.com>
2020-04-02 20:01:08 +0900

    Move the hero

--- hero.js
+++ hero.js
@@ -1,27 +1,33 @@
 const bg = document.querySelector("#bg");
-const div = document.createElement("div");
+const hero = document.createElement("div");
+let heroLocation = "382.5";
 
 function turnFront() {
-    div.className = "hero";
-    bg.append(div);    
+    hero.className = "hero";
+    hero.style.left = `${heroLocation}px`;
+    bg.appendChild(hero);    
 }
 
 function init() {
     turnFront();
     
     document.addEventListener("keydown", function(e){
-        if (e.keyCode === 37 && div.offsetLeft > 0) {
-            // 왼쪽
-            div.style.background = "url('./images/hero.png') 70px";
-        } else if (e.keyCode === 38) {
-            // 위
-            div.style.background = "url('./images/hero.png') 105px"; 
-        } else if (e.keyCode === 39) {
-            // 오른쪽
-            div.style.background = "url('./images/hero.png') 35px"; 
-        } else if (e.keyCode === 40) {
-            // 아래
-            div.style.background = "url('./images/hero.png') 140px"; 
+        if (e.keyCode === 37) { // 왼쪽
+            // console.log('왼쪽');
+            hero.style.background = "url('./images/hero.png') 70px";
+            heroLocation -= 5;
+            hero.style.left = `${heroLocation}px`;
+        }  else if (e.keyCode === 39) { // 오른쪽
+            // console.log('오른쪽');
+            hero.style.background = "url('./images/hero.png') 35px"; 
+            heroLocation += 5;
+            hero.style.left = `${heroLocation}px`;
+        }
+        
+        if (e.keyCode === 38) { // 위
+            hero.style.background = "url('./images/hero.png') 105px"; 
+        } else if (e.keyCode === 40) { // 아래
+            hero.style.background = "url('./images/hero.png') 140px"; 
         }
     });
 
