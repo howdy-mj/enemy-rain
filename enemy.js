5ed248223805c6632827af1a7667a2b7d051fe3a
mj <hi.minjungkim@gmail.com>
2020-04-03 13:48:37 +0900

    Do enemies falling down

--- enemy.js
+++ enemy.js
@@ -1,26 +1,21 @@
-let enemy = document.createElement("div");
-let enemyLocationX = Math.random() * 755;
-let enemyLocationY = 0;
-
-function enemyAppear() {
+const enemyAppear = () => {
+    let enemy = document.createElement("div");
     enemy.className = "enemy";
-    enemy.style.left = `${enemyLocationX}px`;
-    enemy.style.top = `${enemyLocationY}px`;
-    bg.appendChild(enemy);    
-}
+    enemy.style.left = Math.random() * 755 + "px";
+    enemy.style.top = 0 +"px";
+    bg.appendChild(enemy);
 
-const enemyMoveDown = () => {
-    // console.log('5초');
-    if (enemy.offsetTop >= 525) { 
-        enemy.className = "enemy die";
+    let enemyLocationY = 0;
+    setInterval(
+        () => {
+         
+        if (enemyLocationY >= 525) { 
+            enemy.className = "enemy die";
+        }
+        enemyLocationY += 5;
+        enemy.style.top = `${enemyLocationY}px`;
     }
-    enemyLocationY += 5;
-    enemy.style.top = `${enemyLocationY}px`;
-    // console.log(enemy.style.top);
+,100);
 }
 
-function init() {
-    enemyAppear();
-    setInterval( setInterval(enemyMoveDown, 20), 10);
-}
-init();
+setInterval(enemyAppear, 3000);
