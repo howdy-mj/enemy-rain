6444b24ccb3d0e3ebe94a84782cdaef20012bde8
mj <hi.minjungkim@gmail.com>
2020-04-03 23:05:04 +0900

    Enemies disappear when touch bottom -2

--- enemy.js
+++ enemy.js
@@ -10,22 +10,23 @@ const enemyAppear = () => {
     setInterval( () => {
         if (enemyLocationY >= 540) { 
             enemy.className = "enemy die";
-            if (enemy.style.top = 560 + "px") { 
-                bg.removeChild(enemy);
+            if (enemy.style.top = 550 + "px") { 
+                enemy.className = "enemy die";
+                // setTimeout(bg.removeChild(enemy), 3000);
             }
         }
         enemyLocationY += 5;
         enemy.style.top = `${enemyLocationY}px`;
         // console.log(enemyLocationY);
-
+        
         if (enemyLocationY >= 510) {
         // console.log(`두 값의 차이는 ${hero.offsetLeft - enemy.offsetLeft}`);
             if (hero.offsetLeft - enemy.offsetLeft <= 50  && hero.offsetLeft - enemy.offsetLeft >= -50) {
                 enemy.className = "enemy die";
-
                 if (enemyLocationY >= 555) {
                     bg.removeChild(enemy);
                 }
+                
             }
         }
     }, 100);
